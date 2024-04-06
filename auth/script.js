720fbd2e40713a3dd11dcc41ed6e5e36f89616b3
Hunter Pruett <hunterpruett2003@gmail.com>
2024-04-05 22:05:37 -0700

    initial login system done for now

--- auth/script.js
+++ auth/script.js
@@ -13,19 +13,25 @@ function login() {
 
   //grab the element that is child of the sign in button with classname spinner
   let spinner = document.querySelector(".spinner");
-  //show the spinner
+  let errorP = document.getElementById("login-error");
+  let loginButton = document.getElementById("login-button");
 
-  console.log;
+  //show the spinner
   spinner.style.display = "block";
+  errorP.style.display = "none";
+  loginButton.disabled = true;
 
   //delay for 1 second
   setTimeout(() => {
     const authResult = authenticate(username, password);
     spinner.style.display = "none";
+    loginButton.disabled = false;
 
     if (authResult) {
-      window.location.href = "/dashboard";
+      window.location.href = "/index.html";
     } else {
+      errorP.style.display = "block";
+      errorP.innerText = "Invalid username or password";
     }
   }, 1000);
 }
