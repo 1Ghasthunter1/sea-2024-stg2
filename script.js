a29db0edbacc16e19b8db3dbe7f4aab5684628ad
Hunter Pruett <hunterpruett2003@gmail.com>
2024-04-07 10:34:49 -0700

    revised rotation logic

--- script.js
+++ script.js
@@ -9,23 +9,31 @@ if (!window.localStorage.getItem("auth")) {
 }
 
 function rotateElement(event, element) {
-  // get mouse position
   const x = event.clientX;
   const y = event.clientY;
-  // console.log(x, y)
 
-  // find the middle
+  var cloudContainer = document.querySelector("#cloud-container");
+
+  //get low and high x coords and low and high y coords of cloudContainer
+  var cloudContainerRect = cloudContainer.getBoundingClientRect();
+
+  if (
+    !(
+      x >= cloudContainerRect.left &&
+      x <= cloudContainerRect.right &&
+      y >= cloudContainerRect.top &&
+      y <= cloudContainerRect.bottom
+    )
+  ) {
+    return;
+  }
+
   const middleX = window.innerWidth / 2;
   const middleY = window.innerHeight / 2;
-  // console.log(middleX, middleY)
 
-  // get offset from middle as a percentage
-  // and tone it down a little
   const offsetX = ((x - middleX) / middleX) * 45;
   const offsetY = ((y - middleY) / middleY) * 45;
-  // console.log(offsetX, offsetY);
 
-  // set rotation
   element.style.setProperty("--rotateX", offsetX + "deg");
   element.style.setProperty("--rotateY", -1 * offsetY + "deg");
 }
